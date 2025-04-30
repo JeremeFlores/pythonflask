8843717f9f68df4915417eb7ac0034433ea794c7
JeremeFlores <flores.jereme@cec.edu.ph>
2025-04-30 09:45:43 +0800

    add templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Railway!"
+    return render_template('index.html')  # this will load templates/home.html
 
 if __name__ == '__main__':
     app.run(debug=True)
